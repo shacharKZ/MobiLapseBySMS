aa9940220496115edeb1449f0c6c884dd74851db
Mor Paz <1912.mor@gmail.com>
2021-11-26 18:14:25 +0200

    basic line follower 2

--- car_dir.py
+++ car_dir.py
@@ -2,23 +2,27 @@
 import PCA9685 as servo
 import time                # Import necessary modules
 
+
+TURN_15, TURN_25, TURN_35, TURN_45, TURN_60 = 20, 30, 42, 55, 72
+pwm = servo.PWM() 
+
 def Map(x, in_min, in_max, out_min, out_max):
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 def setup_direction(busnum=None):
 	global leftPWM, rightPWM, homePWM, pwm
+	global left_15, left_30, left_45, right_15, right_30, right_45
+	print("setting up dir: ", end="")
 	leftPWM = 400
 	homePWM = 450
 	rightPWM = 500
-	offset =0
+	offset =0  # the offest is taken from the gile "config" (line 3 is the offest we looking for)
 	try:
 		for line in open('config'):
 			if line[0:8] == 'offset =':
 				offset = int(line[9:-1])
 	except:
 		print('config error')
-	# custom val
-	offset=-50
 	print(f'offset is: {offset}')
 	leftPWM += offset
 	homePWM += offset
@@ -38,12 +42,20 @@ def turn_left():
 	global leftPWM
 	pwm.write(0, 0, leftPWM)  # CH0
 
+def turn_left(added_angle = 0):
+	global homePWM, pwm
+	pwm.write(0, 0, homePWM-added_angle) 
+
 # ==========================================================================================
 # Make the car turn right.
 # ==========================================================================================
 def turn_right():
 	global rightPWM
 	pwm.write(0, 0, rightPWM)
+
+def turn_right(add_angle = 0):
+	global homePWM, pwm
+	pwm.write(0, 0, homePWM + add_angle)
 
 # ==========================================================================================
 # Make the car turn back.
@@ -53,8 +65,8 @@ def turn(angle):
 	angle = Map(angle, 0, 255, leftPWM, rightPWM)
 	pwm.write(0, 0, angle)
 
-def home():
-	global homePWM
+def home(dummy_param = None):
+	global homePWM, pwm
 	pwm.write(0, 0, homePWM)
 
 def calibrate(x):
@@ -70,8 +82,30 @@ def test():
 		time.sleep(1)
 		home()
 
-if __name__ == '__main__':
+def test2():
 	setup_direction()
 	home()
+	time.sleep(1.5)
+	for a in [TURN_15, TURN_25, TURN_35, TURN_45, TURN_60]:
+		turn_left(a)
+		time.sleep(1.5)
+	
+	for a in [TURN_15, TURN_25, TURN_35, TURN_45, TURN_60]:
+		turn_right(a)
+		time.sleep(1.5)
+	
+	# turn_left(TURN_15)
+	# time.sleep(1.5)
+	# turn_right()
+	# time.sleep(1.5)
+	home()
 
+if __name__ == '__main__':
+	setup_direction()
+	test2()
+
+	# for i in range(homePWM, leftPWM-100, -10):
+	# 	print(i)
+	# 	time.sleep(1)
+	print("done")
 
