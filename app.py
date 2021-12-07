30c6c1e7e64de494462764fb72f2fccf38feb59b
Mor Paz <1912.mor@gmail.com>
2021-12-07 10:12:00 +0200

    Test all features active

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_cors import CORS, cross_origin
 from capture_handler import create_capture_folders
 from config import STORAGE_BUCKET, ROOT_CAPTURES_FOLDER_PATH
 from file_uploader import upload_image
-# from robot_control import follow_line
+from robot_control import follow_line
 
 # TODO: something in the more complex features is blocking API requests, need to figure out what
 
@@ -31,33 +31,33 @@ def get_command_from_app_in_get():
     return {'message': 'all good from get capture!'}, 200
 
 
-# @app.post('/capture')
-# @cross_origin()
-# def get_command_from_app(args):
-#     global ACTIVE_THREAD
-#     # data = request.get_json()
-#     # print(data)
-#     # num_objects = data.get(['numObjects'], 3)
-#     num_objects = args.get(['numObjects'], 3)
-#     session_timestamp = create_capture_folders(num_objects)
-#     # if data['command'] == 'start':
-#     if args['command'] == 'start':
-#         ACTIVE_THREAD = multiprocessing.Process(target=follow_line, args=(num_objects, session_timestamp))
-#         ACTIVE_THREAD.start()
-#     # elif data['command'] == 'stop':
-#     elif args['command'] == 'stop':
-#         ACTIVE_THREAD.terminate()
-#         upload_new_captures(session_timestamp)
-#         send_convert_request_to_server()
-#     return {'message': 'all good from capture posttt!'}, 200
-#
-#
-# def upload_new_captures(session_timestamp: str):
-#     for dirpath, _, files in os.walk(ROOT_CAPTURES_FOLDER_PATH):
-#         for file in files:
-#             print(os.path.join(dirpath, file))
-#             print(file)
-#             # upload_image(file, os.path.join(dirpath, file))
+@app.post('/capture')
+@cross_origin()
+def get_command_from_app(args):
+    global ACTIVE_THREAD
+    # data = request.get_json()
+    # print(data)
+    # num_objects = data.get(['numObjects'], 3)
+    num_objects = args.get(['numObjects'], 3)
+    session_timestamp = create_capture_folders(num_objects)
+    # if data['command'] == 'start':
+    if args['command'] == 'start':
+        ACTIVE_THREAD = multiprocessing.Process(target=follow_line, args=(num_objects, session_timestamp))
+        ACTIVE_THREAD.start()
+    # elif data['command'] == 'stop':
+    elif args['command'] == 'stop':
+        ACTIVE_THREAD.terminate()
+        upload_new_captures(session_timestamp)
+        send_convert_request_to_server()
+    return {'message': 'all good from capture posttt!'}, 200
+
+
+def upload_new_captures(session_timestamp: str):
+    for dirpath, _, files in os.walk(ROOT_CAPTURES_FOLDER_PATH):
+        for file in files:
+            print(os.path.join(dirpath, file))
+            print(file)
+            # upload_image(file, os.path.join(dirpath, file))
 
 
 def send_convert_request_to_server():
