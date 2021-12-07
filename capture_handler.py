5d83690e8ef4fb3262e696ff176b85586532fd47
Mor Paz <1912.mor@gmail.com>
2021-12-07 10:22:01 +0200

    Test standard cv import

--- capture_handler.py
+++ capture_handler.py
@@ -1,5 +1,7 @@
 import os
 
+import cv2
+
 from datetime import datetime
 
 from config import ROOT_CAPTURES_FOLDER_PATH, API_REQUEST_DATETIME_FORMAT, CAMERA_PATH
@@ -15,17 +17,16 @@ def create_capture_folders(num_objects: int = 3):
     return curr_session_timestamp_string
 
 
-def take_a_pic(curr_object_num: int, curr_picture_num: int, session_timestamp_string: str):
-    pass
-
 # def take_a_pic(curr_object_num: int, curr_picture_num: int, session_timestamp_string: str):
-#     import cv2
-#     cap = cv2.VideoCapture(0)
-#     ret, frame = cap.read()
-#     target_path = os.path.join(ROOT_CAPTURES_FOLDER_PATH,
-#                                f'object{curr_object_num}CaptureSession-{session_timestamp_string}')
-#     pic_label = f'{curr_picture_num}.png'
-#     print(f"Picture attempt result: {ret}, Resulting picture name: {pic_label}")
-#     if not ret:
-#         return False
-#     return cv2.imwrite(target_path + pic_label, frame)
+#     pass
+
+def take_a_pic(curr_object_num: int, curr_picture_num: int, session_timestamp_string: str):
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    target_path = os.path.join(ROOT_CAPTURES_FOLDER_PATH,
+                               f'object{curr_object_num}CaptureSession-{session_timestamp_string}')
+    pic_label = f'{curr_picture_num}.png'
+    print(f"Picture attempt result: {ret}, Resulting picture name: {pic_label}")
+    if not ret:
+        return False
+    return cv2.imwrite(target_path + pic_label, frame)
