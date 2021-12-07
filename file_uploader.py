6567499c67fee6531dd22bb2fb8f34dc7f5f1057
Mor Paz <1912.mor@gmail.com>
2021-12-07 14:30:22 +0200

    Add correct content type

--- file_uploader.py
+++ file_uploader.py
@@ -1,8 +1,10 @@
 from firebase_admin import storage
 
+IMAGE_CONTENT_TYPE = 'image/png'
+
 bucket = storage.bucket()
 
 def upload_image(video_name, video_path):
     blob = bucket.blob(f'robotImages/{video_name}')
     # logger.warning(f'Uploading {video_name} to captures/{video_name} from {video_path}')
-    blob.upload_from_filename(video_path, content_type=VIDEO_CONTENT_TYPE)
+    blob.upload_from_filename(video_path, content_type=IMAGE_CONTENT_TYPE)
