9c250ddd98f3f704914362ea2f1c1e3763dcaa61
Mor Paz <1912.mor@gmail.com>
2021-12-07 10:18:55 +0200

    Test no import of cv func

--- capture_handler.py
+++ capture_handler.py
@@ -16,13 +16,16 @@ def create_capture_folders(num_objects: int = 3):
 
 
 def take_a_pic(curr_object_num: int, curr_picture_num: int, session_timestamp_string: str):
-    import cv2
-    cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()
-    target_path = os.path.join(ROOT_CAPTURES_FOLDER_PATH,
-                               f'object{curr_object_num}CaptureSession-{session_timestamp_string}')
-    pic_label = f'{curr_picture_num}.png'
-    print(f"Picture attempt result: {ret}, Resulting picture name: {pic_label}")
-    if not ret:
-        return False
-    return cv2.imwrite(target_path + pic_label, frame)
+    pass
+
+# def take_a_pic(curr_object_num: int, curr_picture_num: int, session_timestamp_string: str):
+#     import cv2
+#     cap = cv2.VideoCapture(0)
+#     ret, frame = cap.read()
+#     target_path = os.path.join(ROOT_CAPTURES_FOLDER_PATH,
+#                                f'object{curr_object_num}CaptureSession-{session_timestamp_string}')
+#     pic_label = f'{curr_picture_num}.png'
+#     print(f"Picture attempt result: {ret}, Resulting picture name: {pic_label}")
+#     if not ret:
+#         return False
+#     return cv2.imwrite(target_path + pic_label, frame)
