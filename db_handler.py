9ab9430af67ad421eec0e1e7f4bf8ca385f62287
Mor Paz <1912.mor@gmail.com>
2022-01-18 15:23:45 +0200

    Update anomaly data schema

--- db_handler.py
+++ db_handler.py
@@ -21,6 +21,7 @@ def reset_db_state_before_robot_api_start():
     clear_robot_error_in_db()
     reset_anomalies()
 
+
 def reset_db_state_before_capture_start_and_set_capture_state():
     """
     Resets the DB before the robot begins a new capture
@@ -38,13 +39,7 @@ def reset_db_state_before_capture_start_and_set_capture_state():
 def reset_anomalies():
     print('getting DB reference')
     ref = db.reference('/AnomalyData')
-    snapshot = ref.order_by_key().get()
-    for index, value in enumerate(snapshot):
-        if value is None:
-            continue
-        print(f'index: {index}, value: {value}')
-        ref.update({f'{index}/Detected': False,
-                    f'{index}/lastUpdated': str(datetime.datetime.now())})
+    ref.set({'index': -1})
 
 
 def write_api_address_to_db():
@@ -70,8 +65,9 @@ def update_robot_state_in_db(state: int):
 
 def update_anomaly_for_object_in_db(curr_object_num: int):
     print('getting DB reference')
-    ref = db.reference(f'/AnomalyData/{curr_object_num}')
+    ref = db.reference(f'/AnomalyData')
     data = {"Detected": True,
+            "index": curr_object_num + 1,
             "lastUpdated": str(datetime.datetime.now())}
     ref.set(data)
     print(f'Update anomaly data: {data}')
