b9ba07cdfebc3e3fb6a562af0e9a137ace12250e
Shachar Katz <shachar373@gmail.com>
2022-01-18 14:15:44 +0200

    sdfghgfdswqegfhjgfdsfghg

--- QTR_8RC.py
+++ QTR_8RC.py
@@ -82,7 +82,7 @@ def check_above_line():
             f'all ir sensors sees the line with possible_stop_line {possible_stop_line}')
         return res_str
     elif max(res) < 60:
-        if max(res) >= 2.4*min(res):
+        if max(res) >= 2.6*min(res):
             min_color = max(res)*0.9
             possible_stop_line = max(res)*0.9
     elif max(res) > 2*min(res):
